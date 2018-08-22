38e864c170929f776c80064098c23c564dc09c8a
Luke Chi <luke.chi@gmail.com>
2018-08-22 15:32:19 +0800

    adjust build.gradle add HelloApp.java

---
+++ app/src/main/java/com/lukechi/android/hellodagger/HelloApp.java
@@ -0,0 +1,29 @@
+package com.lukechi.android.hellodagger;
+
+import android.app.Activity;
+import android.app.Application;
+
+import javax.inject.Inject;
+
+import dagger.android.DispatchingAndroidInjector;
+import dagger.android.HasActivityInjector;
+import com.lukechi.android.hellodagger.di.AppInjector;
+
+/**
+ * 幾乎不需要異動 可以直接用在不同專案
+ */
+public class HelloApp extends Application implements HasActivityInjector {
+    @Inject
+    DispatchingAndroidInjector<Activity> dispatchingAndroidInjector;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        AppInjector.init(this);
+    }
+
+    @Override
+    public DispatchingAndroidInjector<Activity> activityInjector() {
+        return dispatchingAndroidInjector;
+    }
+}

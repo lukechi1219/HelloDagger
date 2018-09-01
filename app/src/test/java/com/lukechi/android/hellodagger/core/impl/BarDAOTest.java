f9e68b652d55e86114761044ca20ff5d026a9fcf
Luke Chi <luke.chi@gmail.com>
2018-09-01 11:29:52 +0800

    add test

--- app/src/test/java/com/lukechi/android/hellodagger/core/impl/BarDAOTest.java
+++ app/src/test/java/com/lukechi/android/hellodagger/core/impl/BarDAOTest.java
@@ -1,12 +1,27 @@
 package com.lukechi.android.hellodagger.core.impl;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 
+@RunWith(MockitoJUnitRunner.class)
 public class BarDAOTest {
+
+    @Mock
+    FooConfig fooConfig;
 
     @Test
     public void queryAll() {
+
+        when(fooConfig.getIp()).thenReturn("abc");
+
+        BarDAO barDAO = new BarDAO(fooConfig);
+        String data = barDAO.queryAll();
+
+        assertEquals("DATA from abc", data);
     }
 }

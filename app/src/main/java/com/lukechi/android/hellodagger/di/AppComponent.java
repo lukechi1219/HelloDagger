a72989cfa05a1a5f06931349772f6bc6d120b2bc
Luke Chi <luke.chi@gmail.com>
2019-01-18 08:41:18 +0800

    revise use field inject

--- app/src/main/java/com/lukechi/android/hellodagger/di/AppComponent.java
+++ app/src/main/java/com/lukechi/android/hellodagger/di/AppComponent.java
@@ -3,15 +3,16 @@ package com.lukechi.android.hellodagger.di;
 import com.lukechi.android.hellodagger.HelloApp;
 import com.lukechi.android.hellodagger.di.module.ActivityModule;
 import com.lukechi.android.hellodagger.di.module.AppModule;
-
-import javax.inject.Singleton;
-
 import dagger.Component;
 import dagger.android.AndroidInjector;
 import dagger.android.support.AndroidSupportInjectionModule;
 
+import javax.inject.Singleton;
+
 /**
  * setup module bindings 設定 Application 要有哪些 modules
+ *
+ * https://proandroiddev.com/mvp-with-dagger-2-11-847d52c27c5a
  *
  * AndroidSupportInjectionModule includes AndroidInjectionModule
  */
@@ -22,6 +23,14 @@ import dagger.android.support.AndroidSupportInjectionModule;
         ActivityModule.class,
 })
 public interface AppComponent extends AndroidInjector<HelloApp> {
+
+    /**
+     * ref: https://github.com/googlesamples/android-architecture/blob/todo-mvp-dagger/todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/di/AppComponent.java
+     * why need "TasksRepository getTasksRepository()" ?
+     * for test ?
+     */
+//    Heater getHeater(); // 可寫 可不寫?
+//    BazService getBazService(); // 可寫 可不寫?
 
     @Component.Builder
     abstract class Builder extends AndroidInjector.Builder<HelloApp> {}

97bc058c71a0c6a2eb39ed49052024f37139344c
Luke Chi <luke.chi@gmail.com>
2019-01-23 07:51:48 +0800

    update

--- app/src/main/java/com/lukechi/android/opendata/api/AllavailableLotsJson.java
+++ app/src/main/java/com/lukechi/android/opendata/api/AllavailableLotsJson.java
@@ -3,6 +3,8 @@ package com.lukechi.android.opendata.api;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+import java.util.List;
+
 public class AllavailableLotsJson {
 
     @SerializedName("data")
@@ -11,5 +13,103 @@ public class AllavailableLotsJson {
 
     public AllavailableLotsData getData() {
         return data;
+    }
+
+    /**
+     * inner
+     */
+    public static class AllavailableLotsData {
+
+        @SerializedName("UPDATETIME")
+        @Expose
+        String updateTime;
+
+        @SerializedName("park")
+        @Expose
+        List<AllavailableLot> parkingLots;
+
+        public String getUpdateTime() {
+            return updateTime;
+        }
+
+        public List<AllavailableLot> getParkingLots() {
+            return parkingLots;
+        }
+    }
+
+    public static class AllavailableLot {
+
+        @SerializedName("id")
+        @Expose
+        String id;
+
+        @SerializedName("availablecar")
+        @Expose
+        String availableCar;
+
+        @SerializedName("availablemotor")
+        @Expose
+        String availableMotor;
+
+        @SerializedName("availablebus")
+        @Expose
+        String availableBus;
+
+        @SerializedName("ChargeStation")
+        @Expose
+        ChargeStation chargeStation;
+
+        public String getId() {
+            return id;
+        }
+
+        public String getAvailableCar() {
+            return availableCar;
+        }
+
+        public String getAvailableMotor() {
+            return availableMotor;
+        }
+
+        public String getAvailableBus() {
+            return availableBus;
+        }
+
+        public ChargeStation getChargeStation() {
+            return chargeStation;
+        }
+    }
+
+    public static class ChargeStation {
+
+        /**
+         * mother fucker... API field Socket spell wrongly to Scoket...
+         */
+        @SerializedName("scoketStatusList")
+        @Expose
+        List<SocketStatus> socketStatusList;
+
+        public List<SocketStatus> getSocketStatusList() {
+            return socketStatusList;
+        }
+    }
+
+    public static class SocketStatus {
+
+        @SerializedName("spot_abrv")
+        @Expose
+        String spotAbrv;
+
+        @SerializedName("spot_status")
+        @Expose
+        String spotStatus;
+
+        public String getSpotAbrv() {
+            return spotAbrv;
+        }
+
+        public String getSpotStatus() {
+            return spotStatus;
+        }
     }
 }

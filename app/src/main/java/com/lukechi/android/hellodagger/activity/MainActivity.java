a24a76365e7a5089f01171f43170383c2488a17e
Luke Chi <luke.chi@gmail.com>
2019-01-31 05:12:17 +0800

    revise

--- app/src/main/java/com/lukechi/android/hellodagger/activity/MainActivity.java
+++ app/src/main/java/com/lukechi/android/hellodagger/activity/MainActivity.java
@@ -41,9 +41,29 @@ public class MainActivity extends DaggerAppCompatActivity {
         todService.syncAllAvailableLots();
 
         /*
-         * Cannot access database on the main thread since it may potentially lock the UI for a long periods of time.
+          https://developer.android.com/topic/performance/threads
+          For this reason, we suggest that you only use AsyncTask to handle work items shorter than 5ms in duration.
          */
-//        AppDatabase db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "app-database").build();
+
+        /*
+          !!!!!
+          https://medium.com/@cdmunoz/offline-first-android-app-with-mvvm-dagger2-rxjava-livedata-and-room-25de4e1ada14
+          https://proandroiddev.com/offline-first-android-app-with-mvvm-dagger2-rxjava-livedata-and-room-part-2-72716e3520
+         https://medium.com/@cdmunoz/offline-first-android-app-with-mvvm-dagger2-rxjava-livedata-and-room-part-3-af6eeafeb29b
+          https://medium.com/@cdmunoz/offline-first-android-app-with-mvvm-dagger2-rxjava-livedata-and-room-part-4-2b476142e769
+          https://medium.com/@cdmunoz/offline-first-android-app-with-mvvm-dagger2-rxjava-livedata-and-room-part-5-8fc4f9cee34d
+         */
+
+        /*
+         * Cannot access database on the main thread since it may potentially lock the UI for a long periods of time.
+         * Room, by default, won’t allow you to run database operations on the main thread.
+         * -> .allowMainThreadQueries()
+         * Use this approach for testing purposes only, or when dealing with a really tiny database.
+         */
+//        AppDatabase db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "app_database")
+//                .allowMainThreadQueries()
+//                .build();
+//        UserDatabase db = Room.databaseBuilder(getApplicationContext(), UserDatabase.class, "user_db").build();
 //
 //        System.out.println("db: " + db);
 //
@@ -52,5 +72,7 @@ public class MainActivity extends DaggerAppCompatActivity {
 //        for (ParkingLot parkingLot : parkingLotsArray) {
 //            System.out.println(parkingLot.name());
 //        }
+
+
     }
 }
--- app/src/main/java/com/lukechi/android/opendata/database/AppDatabase.java
+++ app/src/main/java/com/lukechi/android/opendata/database/AppDatabase.java
@@ -5,6 +5,18 @@ import androidx.room.RoomDatabase;
 import com.lukechi.android.opendata.database.dao.ParkingLotDao;
 import com.lukechi.android.opendata.database.entity.ParkingLot;
 
+/**
+ * https://medium.com/mindorks/room-with-rxjava-and-dagger-2722f4420651
+ * https://medium.com/mindorks/migration-from-mvp-to-mvvm-using-android-architecture-components-4bc058a1f73c
+ * https://medium.com/mindorks/pagination-using-paging-library-with-rxjava-and-dagger-d9d05dbd8eac
+ * <p>
+ * https://android.jlelse.eu/sqlite-on-android-made-simple-room-persistence-library-with-a-touch-of-rxjava-55e8dc5301cf
+ * <p>
+ * https://android.jlelse.eu/rest-api-on-android-made-simple-or-how-i-learned-to-stop-worrying-and-love-the-rxjava-b3c2c949cad4
+ * https://medium.com/@alahammad/database-with-room-using-rxjava-764ee6124974
+ * <p>
+ * https://github.com/ashwini009/TvMaze/pull/3
+ */
 @Database(
         entities = {ParkingLot.class},
 //        views = {UserDetail.class},
